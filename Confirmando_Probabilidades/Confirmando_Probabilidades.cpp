14be63b22ecb11e4a7b343ac06cf82d74cfd1be1
Gus394 <gustavocoelho394@gmail.com>
2022-03-16 17:52:07 -0300

    1 corrigido e finalizado

--- Confirmando_Probabilidades/Confirmando_Probabilidades.cpp
+++ Confirmando_Probabilidades/Confirmando_Probabilidades.cpp
@@ -1,29 +1,29 @@
 #include <iostream> // Trabalho feito por Gustavo Coelho e João Vitor Custódio na disciplina de Algoritmos e Programação II
 #include <time.h>
 
-int const num_lados = 6; // Não fizemos a leitura do número de lados pois não é permitida a alocação dinâmica do vetor
-
 // Cabeçalho
 int menu();
-void leitura(int &numRepet);
-void contar(int numRepet, float lados[]);
-void escreva(int numRepet, float lados[]);
+void leitura(int& numLados, int& numRepet);
+void contar(int numLados, int numRepet, float contador[]);
+void escrever(int numLados, int numRepet, float contador[]);
 
 int main()
 {
     srand(time(NULL));
 
-    float lados[num_lados] = { 0 };
-    int numRepet = 0;
+    int numLados = 0; // Número de lados do dado
+    int numRepet = 0; // Número de repetições de rolagens
+    float contador[100] = { 0 }; // Um contador que corresponde ao número de vezes que cada lado do dado caiu (máximo de 100 lados)
 
     switch (menu())
     {
     
     case 1:
         
-        leitura(numRepet);
-        contar(numRepet, lados);
-        escreva(numRepet, lados);
+        leitura(numLados, numRepet);
+        std::cout << std::endl;
+        contar(numLados, numRepet, contador);
+        escrever(numLados, numRepet, contador);
         break;
     
     case 2:
@@ -43,7 +43,7 @@ int main()
     }
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int menu() {
     
@@ -56,35 +56,37 @@ int menu() {
     return escolha;
 }
 
-void leitura(int &numRepet) {
+void leitura(int& numLados, int& numRepet) {
     
-    std::cout << "Digite a quantidade de repet: ";
+    std::cout << "Digite a quantidade de lados do dado (entre 1 e 100): ";
+    std::cin >> numLados;
+    std::cout << "Digite a quantidade de repet.: ";
     std::cin >> numRepet;
     return;
 }
 
-void contar(int numRepet, float lados[]) {
+void contar(int numLados, int numRepet, float contador[]) {
     
     int valor = 0;
     for (int i = 0; i < numRepet; i++) {
         
-        valor = rand() % num_lados + 1;
-        lados[valor - 1] ++;
+        valor = rand() % numLados + 1;
+        contador[valor - 1] ++;
     }
     return;
 }
 
-void escreva(int numRepet, float lados[]) {
+void escrever(int numLados, int numRepet, float contador[]) {
     
-    for (int i = 0; i < num_lados; i++) {
-        std::cout << i + 1 << ": " << lados[i] << "\t";
+    for (int i = 0; i < numLados; i++) {
+        std::cout << i + 1 << ": " << contador[i] << "\t";
     }
     
     std::cout << "\n\nDistrib.:\n\n";
     std::cout.precision(3);
     
-    for (int i = 0; i < num_lados; i++) {
-        std::cout << i + 1 << " = " << lados[i] / numRepet * 100 << "%\n";
+    for (int i = 0; i < numLados; i++) {
+        std::cout << i + 1 << " = " << contador[i] / numRepet * 100 << "%\n";
     }
     return;
 }
